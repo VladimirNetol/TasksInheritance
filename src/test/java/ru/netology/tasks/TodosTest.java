d8c03f63c47d9f823288e24a230a88c96552f52c
Vladimir <vladimir_net0logia@mail.ru>
2025-10-23 13:42:14 +0300

    Added a solution

--- src/test/java/ru/netology/tasks/TodosTest.java
+++ src/test/java/ru/netology/tasks/TodosTest.java
@@ -36,7 +36,7 @@ class TodosTest {
         SimpleTask simpleTask = new SimpleTask(5, "Решение домашней работы");
 
         String[] subtasks = {"Сыр", "Колбаса", "Майонез"};
-        Epic epic = new Epic(55, subtasks);
+        Epic epic = new Epic(48, subtasks);
 
         Meeting meeting = new Meeting(
                 37,
@@ -53,6 +53,56 @@ class TodosTest {
 
         Task[] expected = {simpleTask, meeting};
         Task[] actual = todos.search("Решение");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotFindAnyMatches() {
+        SimpleTask simpleTask = new SimpleTask(18, "Зайти в магазин");
+
+        String[] subtasks = {"Буханка", "Фрукты", "Лимонад"};
+        Epic epic = new Epic(76, subtasks);
+
+        Meeting meeting = new Meeting(
+                37,
+                "Построение графика",
+                "Геометрия",
+                "Утром в пятницу"
+        );
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = {};
+        Task[] actual = todos.search("Занятие");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindOneMatch() {
+        SimpleTask simpleTask = new SimpleTask(39, "Пойти в театр");
+
+        String[] subtasks = {"Картофель", "Телефон", "Тетрадь"};
+        Epic epic = new Epic(198, subtasks);
+
+        Meeting meeting = new Meeting(
+                37,
+                "Посмотреть расписание",
+                "Театральная неделя",
+                "В субботу после обеда"
+        );
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = {meeting};
+        Task[] actual = todos.search("неделя");
         Assertions.assertArrayEquals(expected, actual);
     }
 }
